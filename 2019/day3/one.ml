bf75e7b2a5f440f1162888fba6671f929244e239
McKay Marston <mckay.marston@mx.com>
2019-12-08 19:31:12 -0700

    finished day3 part1

--- 2019/day3/one.ml
+++ 2019/day3/one.ml
@@ -17,41 +17,66 @@ let rec unseen node list =
                   then false
                   else unseen node rest
 
+let intersections lines =
+  let first = List.hd lines in
+  let second = List.tl lines
+               |> List.hd in
+  let crosses = ref [] in
+  let rec check node nodes =
+    match nodes with
+    | [] -> !crosses
+    | hd :: rest -> if not (unseen hd first) && not (hd = {x=0; y=0})
+                    then crosses := hd :: !crosses; check hd rest
+  in
+  check {x=0; y=0} second
+
 let walk origin node =
-  Printf.printf "-origin- %d,%d\n" origin.x origin.y;
+  Printf.printf "dir: %c  steps: %d  " node.direction node.steps;
   let path = ref [] in
   let x = origin.x in
   let y = origin.y in
   match node with
-  | {direction='U'; steps} -> for i = 0 to steps do
+  | {direction='U'; steps} -> Printf.printf "U%d\n" steps;
+                              for i = 0 to steps do
                                 if unseen {x=x; y=(y + i)} !path
-                                then path := {x=x; y=(y + i)} :: !path; Printf.printf " U%d: %d, %d\n" steps x (y + i)
+                                then path := {x=x; y=(y + i)} :: !path
                               done; !path
-  | {direction='D'; steps} -> for i = 0 to steps do
+  | {direction='D'; steps} -> Printf.printf "D%d\n" steps;
+                              for i = 0 to steps do
                                 if unseen {x=x; y=(y - i)} !path
-                                then path := {x=x; y=(y - i)} :: !path; Printf.printf " D%d: %d, %d\n" steps x (y - i)
+                                then path := {x=x; y=(y - i)} :: !path
                               done; !path
-  | {direction='L'; steps} -> for i = 0 to steps do
+  | {direction='L'; steps} -> Printf.printf "L%d\n" steps;
+                              for i = 0 to steps do
                                 if unseen {x=(x - i); y=y} !path
-                                then path:= {x=(x - i); y=y} :: !path; Printf.printf " L%d: %d, %d\n" steps (x - i) y
+                                then path:= {x=(x - i); y=y} :: !path
                               done; !path
-  | {direction='R'; steps} -> for i = 0 to steps do
+  | {direction='R'; steps} -> Printf.printf "R%d\n" steps;
+                              for i = 0 to steps do
                                 if unseen {x=(x + i); y=y} !path
-                                then path := {x=(x + i); y=y} :: !path; Printf.printf " R%d: %d, %d\n" steps (x + i) y
+                                then path := {x=(x + i); y=y} :: !path;
                               done; !path
   | _ -> !path
 
+let manhattan_distance point =
+  abs point.x + abs point.y
+
+let closest smallest lst =
+    List.fold_left (fun acc x -> min acc x) smallest lst
+
 let draw line =
+  print_endline "DRAW";
   let path = ref [] in
   let rec work origin node nodes =
     match nodes with
     | [] -> !path
-    | hd :: rest -> path := (walk origin (split hd)) @ !path; work (List.hd !path) hd rest
+    | hd :: rest -> path := (walk origin (split hd)) @ !path;
+                    work (List.hd !path) hd rest
   in
   work {x=0; y=0} "" (String.split_on_char ',' line)
 
 let () =
-  let lines = In_channel.read_lines "test" in
+  let lines = In_channel.read_lines "input" in
   let dimensions = ref [] in
   let rec measure lines =
     match lines with
@@ -59,18 +84,6 @@ let () =
     | hd :: rest -> dimensions := (draw hd) :: !dimensions; measure rest
   in
   measure lines;
-  let print_line line =
-    Printf.printf "origin: %d,%d\n" (List.hd line).x (List.hd line).y;
-    List.iter (fun p -> Printf.printf "  %d, %d\n" p.x p.y) line
-    (* Printf.printf "%d, %d\n" line.x line.y *)
-  in
-  (* print_line ["no"] *)
-  List.iter print_line !dimensions
-  (* for i = 0 to List.length dimensions do
-   *   let line = dimensions.(i) in
-   *   Printf.printf "list length: %d\n" (List.length line)
-   *   (\* for j = 0 to List.length dimensions.[i] do
-   *    *   Printf.printf "-> %d,%d\n" dimensions.[i].[j]
-   *    * done *\)
-   * done
-   * (\* List.map (Printf.printf "Result: %d\n") dimensions.[0] *\) *)
+  let ints = intersections !dimensions in
+  List.iter (fun p -> Printf.printf " => %d, %d: %d\n" p.x p.y (manhattan_distance p)) ints;
+  Printf.printf "closest: %d\n" (closest 10000 (List.map manhattan_distance ints))
