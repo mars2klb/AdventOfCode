00fa51d61ae901e4bba35a0e6d0f8081d9d4a915
McKay Marston <mckay.marston@mx.com>
2019-12-17 13:34:15 -0700

    updated of day5

--- 2019/day5/one.ml
+++ 2019/day5/one.ml
@@ -1,6 +1,12 @@
 let run stream =
-  let rec compute head = match stream.(head) with
-    | 99 -> stream
+  let rec compute head =
+    let modes = stream.(head) in
+    let mode3 = modes / 10000 in
+    let mode2 = (modes mod 10000) / 1000 in
+    let mode1 = (modes mod 1000) / 100 in
+    let opcode = modes mod 100 in
+    match opcode with
+    | 99 -> print_endline "HALT"; stream
     | 1 -> let lhs = stream.(head + 1) in
            let rhs = stream.(head + 2) in
            let dst = stream.(head + 3) in
@@ -13,6 +19,8 @@ let run stream =
            compute (head + 4)
     | 3 -> let arg = stream.(head + 1) in
            compute (head + 3)
+    | 4 -> let pos = stream.(head + 1) in
+           stream.(pos) <- compute (head + 3)
     | _ -> raise Exit
   in
   compute 0
