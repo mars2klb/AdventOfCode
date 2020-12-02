5af7f7f84f1f9276c447898972d47f30ce89e201
smazga <smazga@greymanlabs.com>
2020-12-01 21:32:29 -0700

    2020 day1 part2

--- 2020/day1/one.ml
+++ 2020/day1/one.ml
@@ -1,10 +1,10 @@
-let test l r = (l + r = 2020)
+let equal2020 l r = (l + r = 2020)
 
-let find var data =
+let find_equal var data =
   let rec look_for item within =
   match within with
   | hd :: tl ->
-     (let rhs = List.find_opt (fun x -> x != hd && test x hd) data in
+     (let rhs = List.find_opt (fun x -> x != hd && equal2020 x hd) data in
       match rhs with
       | Some value -> (hd, value)
       | None -> look_for item tl)
@@ -15,6 +15,6 @@ let find var data =
 let () =
   let data = In_channel.read_lines "input"
            |> List.map int_of_string in
-  match (find (List.hd data) data) with
+  match (find_equal (List.hd data) data) with
   | (match1, match2) when (match1 * match2 != 0) ->  print_endline ("Part 1: " ^ string_of_int match1 ^ " <-> " ^ string_of_int match2 ^ " :: " ^ string_of_int (match1 * match2))
   | _ -> raise (Failure "not found")
---
+++ 2020/day1/two.ml
@@ -0,0 +1,25 @@
+let lessThan2020 l r = l + r < 2020
+
+let find_less_than var data =
+  let results = ref [] in
+  let rec look_for item within =
+    match within with
+    | hd :: tl ->
+        let matches = List.filter (fun x -> x != hd && lessThan2020 x hd) data in
+        List.iter (fun x -> results := !results @ [(x, hd)]) matches ;
+        look_for item tl
+    | [] -> !results in
+  look_for var data
+
+let rec solve tup data =
+  let l, r = tup in
+  match data with
+  | hd :: tl when hd + l + r = 2020 -> hd * l * r
+  | _ :: tl -> solve tup tl
+  | [] -> 0
+
+let () =
+  let data = In_channel.read_lines "input" |> List.map int_of_string in
+  let potentials = List.flatten (List.map (fun x -> find_less_than x data) data) in
+  let answer = List.filter (fun x -> x > 0) (List.map (fun x -> solve x data) potentials) in
+  print_endline ("Part 2: " ^ string_of_int (List.hd answer))
