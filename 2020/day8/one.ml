bf9faf2c018de352b42e9ea606c815c3fb0f08ad
smazga <smazga@greymanlabs.com>
2020-12-08 21:28:47 -0700

    2020 day 8 parts 1 and 2

--- 2020/day8/one.ml
+++ 2020/day8/one.ml
@@ -1,2 +1,24 @@
+let parse opcode =
+  if Str.string_match (Str.regexp "\\([a-z]+\\) \\([+|-][0-9]+\\)") opcode 0
+  then
+    let code = Str.matched_group 1 opcode in
+    let delta = int_of_string (Str.matched_group 2 opcode) in
+    (code, delta)
+  else
+    raise (Failure "parsing failure")
+
+let rec compute opcodes cmd trail =
+  let offset, acc = cmd in
+  try
+    let _ = List.find (fun x -> x = offset) trail in acc
+  with Not_found -> ();
+  let code, delta = parse (List.nth opcodes offset) in
+  match code with
+  | "nop" -> compute opcodes (offset + 1, acc) (trail @ [offset])
+  | "acc" -> compute opcodes (offset + 1, acc + delta) (trail @ [offset])
+  | "jmp" -> compute opcodes (offset + delta, acc) (trail @ [offset])
+  | _ -> raise (Failure "bad opcode")
+
 let () =
-  let data = In_channel.read_lines "test" in
+  let data = In_channel.read_lines "input" in
+  print_endline ("Part 1: last good acc: " ^ string_of_int (compute data (0, 0) []))
--- 2020/day8/two.ml
+++ 2020/day8/two.ml
@@ -1,2 +1,33 @@
+let rec compute opcodes cmd trail =
+  let offset, acc = cmd in
+  (try
+    let _ = List.find (fun x -> x = offset) trail in
+    raise (Failure "infinite loop")
+  with Not_found -> ());
+  if offset = List.length opcodes then acc
+  else
+    let code, delta = One.parse (List.nth opcodes offset) in
+     match code with
+     | "nop" -> compute opcodes (offset + 1, acc) (trail @ [offset])
+     | "acc" -> compute opcodes (offset + 1, acc + delta) (trail @ [offset])
+     | "jmp" -> compute opcodes (offset + delta, acc) (trail @ [offset])
+     | _ -> acc
+
+let flip opcode =
+  let code, delta = One.parse opcode in
+  match code with
+  | "nop" -> "jmp " ^ string_of_int delta
+  | "jmp" -> "nop " ^ string_of_int delta
+  | _ -> opcode
+
+let rec churn opcodes offset =
+  if offset > ((List.length opcodes) - 1) then raise (Failure "nothing found");
+  let iteration = List.mapi (fun idx code -> if idx = offset then flip code else code) opcodes in
+  try
+    compute iteration (0, 0) []
+  with Failure _ ->
+    churn opcodes (offset + 1)
+
 let () =
-  let data = In_channel.read_lines "test" in
+  let data = In_channel.read_lines "input" in
+  print_endline ("Part 2: last good acc: " ^ string_of_int (churn data 0))
