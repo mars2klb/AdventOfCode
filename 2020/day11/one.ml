b6559c23291eeccea90c0d11047c6117eba0b6b9
smazga <smazga@greymanlabs.com>
2020-12-11 16:46:29 -0700

    2020 day 11 part 1, part 2 wip

--- 2020/day11/one.ml
+++ 2020/day11/one.ml
@@ -1,55 +1,63 @@
-let dump seat =
-  let center, left, right, up, down = seat in
-  print_endline (" " ^ up " ");
-  print_endline (left ^ center ^ right);
-  print_endline (" " ^ down ^ " ")
+exception Out_of_bounds
 
-let dump_seats seats =
-  List.iter print_endline seats
+let dump seat =
+  let x, y = seat in
+  "(" ^ string_of_int x ^ ", " ^ string_of_int y ^ ")"
+
+let dump_seats seats = String.concat "\n" seats
 
 let get_seat coord seats =
   let x, y = coord in
   try
     let line = List.nth seats y in
     line.[x]
-  with
-  | Failure _
-  | Invalid_argument _ -> '.'
+  with Failure _ | Invalid_argument _ -> raise Out_of_bounds
 
-let get_neighbor center direction seats =
-  let cx, cy = center in
+let seat_coord start direction =
+  let x, y = start in
   let dx, dy = direction in
-  get_seat (cx + dx, cy + dy) seats
+  (x + dx, y + dy)
+  
+let get_neighbor center direction seats =
+  try
+    get_seat (seat_coord center direction) seats
+  with Out_of_bounds -> '.'
+
+let rec get_line_of_sight_neighbor center direction seats =
+  let seat = get_neighbor center direction seats in
+  match seat with
+  | '.' -> get_line_of_sight_neighbor (seat_coord center direction) direction seats
+  | s -> s
 
 let rec occupied input output =
-  match input with
-  | '#' :: tl -> occupied tl (output + 1)
-  | _ :: tl -> occupied tl output
-  | [] -> output
+  match input with '#' :: tl -> occupied tl (output + 1) | _ :: tl -> occupied tl output | [] -> output
 
-let neighbors seat seats =
-  let coords = [(-1, 0);  (* left *)
-                (1, 0);   (* right *)
-                (0, -1);  (* up *)
-                (-1, -1); (* up/left *)
-                (1, -1);  (* up/right *)
-                (0, 1);   (* down *)
-                (-1, 1);  (* down/left *)
-                (1, 1)    (* down/right *)
-               ] in
+let neighbors tolerance seat seats =
+  let coords =
+    (* left, right, up, up/left, up/right, down, down/left, down/right *)
+    [(-1, 0); (1, 0); (0, -1); (-1, -1); (1, -1); (0, 1); (-1, 1); (1, 1)] in
   let adj = List.map (fun x -> get_neighbor seat x seats) coords in
-  match get_seat seat seats with
-  | 'L' -> if (occupied adj 0) = 0 then '#' else 'L'
-  | '#' -> if (occupied adj 0) >= 4 then 'L' else '#'
+  let mark =
+    try
+      get_seat seat seats
+    with Out_of_bounds -> '.' in
+  (* match get_seat seat seats with *)
+  match mark with
+  | 'L' -> if occupied adj 0 = 0 then '#' else 'L'
+  | '#' -> if occupied adj 0 >= tolerance then 'L' else '#'
   | x -> x
 
-let walk seats =
-  List.mapi (fun x row -> String.mapi (fun y _ -> neighbors (x, y) seats) row) seats
+let walk tolerance seats = List.mapi (fun y row -> String.mapi (fun x _ -> neighbors tolerance (x, y) seats) row) seats
+let tally seats = List.fold_left (fun acc row -> acc + occupied (List.of_seq (String.to_seq row)) 0) 0 seats
+
+let rec churn ?tolerance:(tolerance=4) seats count =
+  let churned = walk tolerance seats in
+  if List.for_all2 (fun a b -> String.equal a b) seats churned then (
+    (* print_endline ("iterations: " ^ string_of_int count) ;
+     * print_endline (dump_seats churned) ; *)
+    tally churned )
+  else churn ~tolerance:tolerance churned (count + 1)
 
 let run () =
-  let seats = Lib.In_channel.read_lines "day11/test" in
-  dump_seats seats;
-  let foo = walk seats in
-  dump_seats foo;
-  (* List.iter (fun x -> dump x) foo; *)
-  "unimplemented"
+  let seats = Lib.In_channel.read_lines "day11/input" in
+  string_of_int (churn seats 0)
--- 2020/day11/two.ml
+++ 2020/day11/two.ml
@@ -1,3 +1,3 @@
 let run () =
-  let _ = Lib.In_channel.read_lines "day11/test" in
-  "unimplemented"
+  let seats = Lib.In_channel.read_lines "day11/test" in
+  string_of_int (One.churn ~tolerance:5 seats 0)
