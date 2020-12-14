36b0c6c53867974ba696a0c93fb13de7debcaeaf
smazga <smazga@greymanlabs.com>
2020-12-14 11:57:11 -0700

    improved day 12 part 2, but still not correct

--- 2020/day12/two.ml
+++ 2020/day12/two.ml
@@ -2,56 +2,37 @@ let ship = ref (0, 0)
 
 let mult x by = (if x = 0 then 1 else x) * by
 
-let delta start finish =
-  print_endline (" DELTA: start:" ^ Ahoy.dump_coord start ^ " finish:" ^ Ahoy.dump_coord finish);
-  let sx, sy = start in
-  let fx, fy = finish in
-  ((abs (sx - fx)), (abs (sy - fy)))
+let turn_left pos =
+  let x, y = pos in (-y, x)
 
-let heading start finish =
-  print_endline (" HEADING: start:" ^ Ahoy.dump_coord start ^ " finish:" ^ Ahoy.dump_coord finish);
-  let sx, sy = start in
-  let fx, fy = finish in
-  (sx - fx, sy - fy)
+let turn_right pos =
+  let x, y = pos in (y, -x)
 
 let rotate_waypoint direction degrees from =
-  let start = String.index Ahoy.cardinals !Ahoy.heading in
-  Ahoy.rotate direction degrees;
-  let wx, wy = (delta !ship from) in
-  let sx, sy = !ship in
-  let foo = match (start + (360 / degrees)) mod 4 with
-    | 0 -> (sx + wy, sy - wx) (* 0 *)
-    | 1 -> (sx - wy, sy + wx) (* 90 *)
-    | 2 -> (sx - wx, sy - wy) (* 180 *)
-    | 3 -> (sx - wx, sy + wy)
-    | _ -> raise (Failure "spinning out of control")
-  in
-  print_endline (" ROTATED: to: " ^ Char.escaped !Ahoy.heading ^ ": " ^ string_of_int degrees ^ "[" ^ string_of_int ((start + (360 / degrees)) mod 4) ^ "] " ^ Ahoy.dump_coord foo);
-  foo
-
+  print_endline ("Rotate from " ^ Char.escaped direction ^ " " ^ string_of_int degrees ^ " " ^ Ahoy.dump_coord from);
+  let x, y = from in
+  let delta = (degrees / 90) mod 4 in
+  if delta mod 2 = 0
+  then (-x, -y)
+  else match direction with
+       | 'R' -> if delta = 1 then turn_right from else turn_left from
+       | 'L' -> if delta = 1 then turn_left from else turn_right from
+       | _ -> raise (Failure "bad waypoint rotation")
+  
 let move_ship toward from =
   let _, distance = toward in
   let cx, cy = !ship in
-  let x, y = (heading from !ship) in
+  let x, y = from in
   let dx, dy = (mult x distance, mult y distance) in
   print_endline (" move ship from: " ^ Ahoy.dump_coord !ship);
   print_endline ("  waypoint " ^ Ahoy.dump_coord from);
   print_endline (" POSITION CHANGE: distance:" ^ string_of_int distance
                  ^ " delta:" ^ string_of_int x ^ ", " ^ string_of_int y);
   print_endline (" POSITION DX: " ^ string_of_int dx ^ ", " ^ string_of_int dy);
-  (* let position = match !Ahoy.heading with
-   * | 'E' -> (cx + dx, cy + dy)
-   * | 'W' -> (cx - dx, cy + dy)
-   * | 'N' -> (cx + dx, cy + dy)
-   * | 'S' -> (cx - dx, cy - dy)
-   * | _ -> raise (Failure "ship lost") *)
-  let position = (cx + dx, cy + dy)
-  in
-  ship := position;
+  let position = (cx + dx, cy + dy) in ship := position;
   print_endline (" ship is at: " ^ Ahoy.dump_coord !ship);
-  let px, py = position in
-  print_endline ("  waypoint end:" ^ Ahoy.dump_coord (x + px, y + py));
-  (x + px, y + py)
+  print_endline ("  waypoint end:" ^ Ahoy.dump_coord from);
+  from
 
 let move toward from =
   let direction, distance = toward in
@@ -72,5 +53,5 @@ let run () =
   let data = Lib.In_channel.read_lines "day12/input" in
   let points = Ahoy.chart data in
   ignore (Ahoy.navigate move ([('E', 10); ('N', 1)] @ points) (0, 0));
-  let x, y = delta (0, 0) !ship in
-  "distance: " ^ string_of_int (x + y)
+  let x, y = !ship in
+  "distance: " ^ string_of_int (abs x + abs y)
